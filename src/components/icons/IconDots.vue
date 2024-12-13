9e08402038b7d45a00ad11da1be4b3cc065c2dca
mke <for.office.gthb@gmail.com>
2024-12-13 15:11:31 +0500

    Added new icons to the icons folder

---
+++ src/components/icons/IconDots.vue
@@ -0,0 +1,17 @@
+<template>
+	<svg
+		data-slot="icon"
+		fill="none"
+		stroke-width="1.5"
+		stroke="currentColor"
+		viewBox="0 0 24 24"
+		aria-hidden="true"
+		class="size-5"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+		></path>
+	</svg>
+</template>

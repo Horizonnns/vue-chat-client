b0849f5b5765de3075c8b4cd0b0e7166ea4ca59b
mke <for.office.gthb@gmail.com>
2024-12-13 15:10:41 +0500

    Translated the code of the App component to vue compositoin api

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
-<template>
-	<div class="container mx-auto p-4">
-		<h1 class="text-2xl font-bold mb-4">Реальный чат</h1>
-		<Chat />
-	</div>
-</template>
-
-<script>
+<script setup>
 import Chat from './components/Chat.vue';
-
-export default {
-	components: { Chat },
-};
 </script>
 
-<style>
-/* TailwindCSS добавит стили автоматически */
-</style>
+<template>
+	<section class="mx-auto rounded-sm w-96 h-screen">
+		<Chat />
+	</section>
+</template>

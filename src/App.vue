b82e5b6040084222b85fa3e1e82e036740664bfa
mke <for.office.gthb@gmail.com>
2024-12-31 20:06:39 +0500

    fix(app): Configured the app component with the router

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <script setup>
-import Chat from './components/Chat.vue';
+import { computed } from 'vue';
+import { useAuthStore } from './stores/auth/auth';
+const authStore = useAuthStore();
+const token = computed(() => authStore.getToken);
 </script>
 
 <template>
-	<section class="mx-auto rounded-sm w-96 h-screen">
-		<Chat />
+	<section
+		class="mx-auto rounded-sm w-96 h-screen flex flex-col justify-between bg-gray-20"
+	>
+		<div class="w-full h-full flex flex-col items-center">
+			<h2
+				v-if="!token"
+				class="bg-gray-20 text-xl text-center font-semibold italic w-full py-3"
+			>
+				Vue-chat
+			</h2>
+
+			<main class="w-full h-full flex items-center justify-center">
+				<RouterView />
+			</main>
+		</div>
 	</section>
 </template>

9cf2d5818e91351ca4b0874ce29483c081903f61
mke <for.office.gthb@gmail.com>
2024-12-31 18:28:55 +0500

    Set color variables in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				gray: {
+					10: '#828282',
+					20: '#1b1b1b',
+				},
+				blue: {
+					10: '#4d8ff1',
+				},
+			},
+		},
 	},
 	plugins: [],
 };
